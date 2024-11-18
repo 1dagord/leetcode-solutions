f05ff5f9b572a9639809febab53ae2b276e30d9d
Devin Gordon <dag334@cornell.edu>
2024-11-18 13:32:14 -0600

    Adding LC 138

---
+++ Solutions/Python/copy_list_with_random_pointer.py
@@ -0,0 +1,47 @@
+"""
+    [MEDIUM]
+    138. Copy List with Random Pointer
+
+    Concepts:
+    - linked list
+    - hash table
+
+    Stats:
+        Runtime | 36 ms     [Beats 70.78%]
+        Memory  | 17.41     [Beats 62.71%]
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        orig_to_cpy = {} # Node_Object -> node.val
+
+        # associate each node object with a
+        # new node of same value
+        curr = head
+        while curr:
+            orig_to_cpy[curr] = Node(curr.val)
+            curr = curr.next
+
+        # link all new nodes together using
+        # `next` and `random` of old nodes
+        curr = head
+        while curr:
+            # .get() : returns value with given key or None if not in dict
+            orig_to_cpy[curr].next = orig_to_cpy.get(curr.next)
+            orig_to_cpy[curr].random = orig_to_cpy.get(curr.random)
+            curr = curr.next
+
+        # return copy of new list
+        return orig_to_cpy[head]
