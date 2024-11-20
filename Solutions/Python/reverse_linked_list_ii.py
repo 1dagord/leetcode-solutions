a2c60244b673e63cade6d2bc168c2e5594ca0c1a
Devin Gordon <dag334@cornell.edu>
2024-11-20 09:19:01 -0600

    Adding LC 92

---
+++ Solutions/Python/reverse_linked_list_ii.py
@@ -0,0 +1,73 @@
+"""
+    [MEDIUM]
+    92. Reverse Linked List II
+
+    Concepts:
+    - linked list
+
+    Stats:
+        Runtime | 0 ms      [Beats 100%]
+        Memory  | 16.70 MB  [Beats 33.44%]
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        """
+            Use LIFO properties of stack to reverse node
+            and splice together reversed portion with
+            prepended and appended list
+        """
+        dummy = ListNode(0, head)
+        pre = dummy
+        prev = dummy
+        curr = dummy
+        post = head
+
+        res = dummy
+
+        if left == right:
+            return head
+
+        # shift in curr and post pointers
+        while (right - left):
+            curr = curr.next
+            post = post.next
+            right -= 1
+        
+        while left:
+            if left > 1:
+                pre = pre.next
+
+            prev = prev.next
+            curr = curr.next
+            post = post.next
+
+            left -= 1
+
+        # add nodes to stack
+        middle_nodes = []
+
+        # get all nodes in between
+        temp = prev
+        while temp != post:
+            middle_nodes.append(temp)
+            temp = temp.next
+
+        # pop nodes off stack and append to list
+        while middle_nodes:
+            node = middle_nodes.pop()
+            pre.next = node
+            pre = pre.next
+
+
+        if post:
+            pre.next = post
+        else:
+            pre.next = None
+
+        return res.next
