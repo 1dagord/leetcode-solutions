baf215b64915f159dac4cac7b392ba894007559c
Devin Gordon <dag334@cornell.edu>
2024-11-20 19:23:44 -0600

    Adding LC 153

---
+++ Solutions/Python/find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,39 @@
+"""
+    [MEDIUM]
+    153. Find Minimum in Rotated Sorted Array
+
+    Concepts:
+    - binary search
+    - array
+
+    Stats:
+        Runtime | 0 ms      [Beats 100%]
+        Memory  | 17.06 MB  [Beats 6.23%]
+"""
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        """
+            Half of array is sorted, other half is rotated
+
+            Identify which half has min, then run Binary Search
+        """
+        n = len(nums)
+        l = 0
+        r = n - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            
+            # if left > right, search right half
+            if nums[l] > nums[r]:
+                # if mid > left > right, search right half
+                if nums[m] >= nums[l]:
+                    l = m + 1
+                # else, search left half
+                else:
+                    r = m
+            else:
+                return nums[l]
+
+        return nums[r]
