5c2929113e944bb6acffc2d8fd3d6a6c75e88031
Devin Gordon <dag334@cornell.edu>
2024-12-16 15:55:59 -0500

    Adding LC 3264

---
+++ Solutions/Python/final_array_state_after_k_multiplication_operations_i.py
@@ -0,0 +1,23 @@
+"""
+    [EASY]
+    3264. Final Array State After K Multiplication Operations I
+
+    Concepts:
+    - heap
+
+    Stats:
+        Runtime | 4 ms      [Beats 26.45%]
+        Memory  | 17.32 MB  [Beats 19.20%]
+"""
+
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        vals = [(nums, i) for i, nums in enumerate(nums)]
+        heapq.heapify(vals)
+        while k:
+            val, i = heapq.heappop(vals)
+            nums[i] = val * multiplier
+            heapq.heappush(vals, (val * multiplier, i))
+            k -= 1
+
+        return nums
