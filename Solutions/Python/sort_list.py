662c9824bf2bae6618bb4384fdc3793e4fcad7c5
Devin Gordon <dag334@cornell.edu>
2025-01-04 13:07:42 -0500

    Adding LC 148

---
+++ Solutions/Python/sort_list.py
@@ -0,0 +1,40 @@
+"""
+    [MEDIUM]
+    148. Sort List
+
+    Concepts:
+    - linked list
+    - merge sort
+
+    Stats:
+        Runtime | 26 ms     [Beats 89.96%]
+        Memory  | 33.01 MB  [Beats 19.91%]
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        nodes = []
+        curr = head
+
+        while curr:
+            nodes.append(curr)
+            curr = curr.next
+
+        for node in nodes:
+            node.next = None
+
+        n = len(nodes)
+
+        # implements timsort under the hood
+        nodes.sort(key=lambda x: x.val)
+        nodes += [None]
+
+        for i in range(n):
+            nodes[i].next = nodes[i+1]
+
+        return nodes[0]
