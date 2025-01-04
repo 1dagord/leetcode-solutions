f12b1f22b7e1bba5279962dca9e4a45ff4b1ef67
Devin Gordon <dag334@cornell.edu>
2025-01-04 13:36:47 -0500

    Added linked list to merge_sort.py

--- DSA/Sorting/merge_sort.py
+++ DSA/Sorting/merge_sort.py
@@ -1,4 +1,11 @@
-def merge(left: list[int], right: list[int]):
+import ultraimport
+
+ListNode = ultraimport("__dir__/../DataStructures/linked_list.py", "ListNode")
+
+
+# ----- List Merge Sort -----
+
+def list_merge(left: list[int], right: list[int]):
 	"""
 		Merges two sublists
 
@@ -27,8 +34,7 @@ def merge(left: list[int], right: list[int]):
 
 	return res
 
-
-def mergeSort(nums: list[int]):
+def list_merge_sort(nums: list[int]):
 	"""
 		Recursively splits list into sublists
 		until sublist size is 1, then rebuilds
@@ -53,7 +59,81 @@ def mergeSort(nums: list[int]):
 			right.append(num)
 
 	# recursively sort sublists
-	left = mergeSort(left)
-	right = mergeSort(right)
+	left = list_merge_sort(left)
+	right = list_merge_sort(right)
 
-	return merge(left, right)
+	return list_merge(left, right)
+
+
+# ----- Linked List Merge Sort -----
+
+def find_middle(head: ListNode):
+	"""
+		Returns middle node of a linked list
+
+		:param head: head of linked list
+
+		:return: ListNode at center of linked list
+	"""
+    slow = head
+    fast = head.next
+
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+    
+    return slow
+
+def linked_list_merge(left: ListNode, right: ListNode):
+	"""
+		Merges two sublists
+
+		:param left: head of left linked sublist
+		:param right: head of right linked sublist
+
+		:return: joined linked list
+	"""
+    l = left
+    r = right
+    dummy = ListNode(-1)
+    curr = dummy
+
+    while l and r:
+        if l.val < r.val:
+            curr.next = l
+            curr = l
+            l = l.next
+        else:
+            curr.next = r
+            curr = r
+            r = r.next
+        
+    curr.next = l if l else r
+
+    return dummy.next
+
+def linked_list_merge_sort(head: ListNode):
+	"""
+		Recursively splits linked list into sublists
+		until sublist size is 1, then rebuilds
+		list from sorted sublists
+
+		:param head: head node of linked list
+					 to be sorted
+
+		:return: sorted linked list
+	"""
+    if not head or not head.next:
+        return head
+
+    middle = find_middle(head)
+    left_head = head
+    right_head = middle.next
+    middle.next = None
+
+    left = linked_list_merge_sort(left_head)
+    right = linked_list_merge_sort(right_head)
+
+    return linked_list_merge(left, right)
+
+return linked_list_merge_sort(head)
