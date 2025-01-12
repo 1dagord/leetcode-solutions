89dcd106051b56e55fa3e2296052ad71c3514640
Devin Gordon <dag334@cornell.edu>
2025-01-11 19:14:04 -0500

    Adding LC 1400

---
+++ Solutions/Python/construct_k_palindrome_strings.py
@@ -0,0 +1,27 @@
+"""
+    [MEDIUM]
+    1400. Construct K Palindrome Strings
+
+    Concepts:
+    - hash table
+    - string
+
+    Stats:
+        Runtime | 27 ms     [Beats 93.55%]
+        Memory  | 18.19 MB  [Beats 19.36%]
+"""
+
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        """
+            String is palindrome every character appears
+            even number of times (except maybe one)
+        """
+        if len(s) < k:
+            return False
+            
+        return (
+            len(
+                [(key, val) for key, val in Counter(s).items() if val % 2 != 0]
+            ) <= k
+        )
