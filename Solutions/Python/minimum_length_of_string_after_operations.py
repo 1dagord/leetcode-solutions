d2e68d15254adcb58728ec9e9122fd41917ff3be
Devin Gordon <dag334@cornell.edu>
2025-01-14 16:17:46 -0500

    Adding LC 3223

---
+++ Solutions/Python/minimum_length_of_string_after_operations.py
@@ -0,0 +1,28 @@
+"""
+    [MEDIUM]
+    3223. Minimum Length of String After Operations
+
+    Concepts:
+    - string
+    - hash table
+
+    Stats:
+        Runtime | 384 ms    [Beats 12.66%]
+        Memory  | 19.02 MB  [Beats 7.08%]
+"""
+
+from collections import Counter
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        """
+            If at least 3 occurrences of char,
+            delete two until less than 3 remain
+        """
+        counter = Counter(s)
+
+        for k in counter:
+            while counter[k] >= 3:
+                counter[k] -= 2
+
+        return sum([v for v in counter.values()])
