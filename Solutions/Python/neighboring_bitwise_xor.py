c91d78e44f55a1aa779c92e3961afa681e0c2939
Devin Gordon <dag334@cornell.edu>
2025-01-17 11:10:51 -0500

    Updating LC 2683

--- Solutions/Python/neighboring_bitwise_xor.py
+++ Solutions/Python/neighboring_bitwise_xor.py
@@ -6,7 +6,7 @@
     - bit manipulation
 
     Stats:
-        Runtime | 260 ms    [Beats 5.11%]
+        Runtime | 211 ms    [Beats 5.11%]
         Memory  | 21.87 MB  [Beats 79.56%]
 """
 
@@ -20,23 +20,14 @@ class Solution:
 
         # construct array based on rule set
         for i in range(n - 1):
-            if derived[i]:
-                original[i+1] = original[i] ^ 1
-            else:
-                original[i+1] = original[i]
+            original[i+1] = original[i] ^ derived[i]
 
-        original[-1] = 1 ^ original[0] if derived[-1] else original[0]
+        original[-1] = original[0] ^ derived[-1]
 
         # verify array
         for i in range(n - 1):
-            if (
-                (derived[i] and original[i] == original[i+1]) or
-                (1 ^ derived[i] and original[i] != original[i+1])
-            ):
+            if (derived[i] != original[i] ^ original[i+1]):
                 return False
 
         # verify last element
-        return (
-            (derived[-1] == (original[-1] != original[0])) or
-            ((1 ^ derived[i]) == (original[-1] == original[0]))
-        )
+        return (derived[-1] == (original[-1] ^ original[0]))
