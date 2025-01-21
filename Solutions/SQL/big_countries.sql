9810adbd4c9cda16ab6410b166ed4c2f02e3a1aa
Devin Gordon <dag334@cornell.edu>
2025-01-21 16:49:22 -0500

    Adding LC 595

---
+++ Solutions/SQL/big_countries.sql
@@ -0,0 +1,15 @@
+/*
+    [EASY]
+    595. Big Countries
+
+    Concepts:
+    - select
+
+    Stats:
+        Runtime | 274 ms    [Beats 85.05%]
+*/
+
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000
+OR population >= 25000000
