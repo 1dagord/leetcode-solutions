fedfa7c2eaca5ad675104f620b94ef46a75d1fe9
Devin Gordon <dag334@cornell.edu>
2025-01-21 16:43:44 -0500

    Adding LC 584

---
+++ Solutions/SQL/find_customer_referee.sql
@@ -0,0 +1,15 @@
+/*
+    [EASY]
+    584. Find Customer Referee
+
+    Concepts:
+    - select
+
+    Stats:
+        Runtime | 623 ms    [Beats 64.26%]
+*/
+
+SELECT name
+FROM Customer
+WHERE referee_id != 2
+OR referee_id IS NULL
