b6e6979486239972d76fed2b7995cfedde6767b4
Devin Gordon <dag334@cornell.edu>
2025-01-21 14:47:13 -0500

    Updating LC 1757

--- Solutions/SQL/recyclable_and_low_fat_products.sql
+++ Solutions/SQL/recyclable_and_low_fat_products.sql
@@ -1,4 +1,11 @@
--- Write your MySQL query statement below
+/*
+    [EASY]
+    1757. Recyclable and Low Fat Products
+
+    Stats:
+        Runtime | 1003 ms   [Beats 29.67%]
+*/
+
 SELECT product_id
 FROM Products
 WHERE low_fats = "Y"
