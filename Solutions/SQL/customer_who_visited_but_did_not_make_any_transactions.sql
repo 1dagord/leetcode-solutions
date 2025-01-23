5f01143a5b0ea3025e1aebdc71fa74918bfd4d69
Devin Gordon <dag334@cornell.edu>
2025-01-23 11:24:02 -0800

    Adding LC 1581

---
+++ Solutions/SQL/customer_who_visited_but_did_not_make_any_transactions.sql
@@ -0,0 +1,17 @@
+/*
+    [EASY]
+    1581. Customer Who Visited but Did Not Make Any Transactions
+
+    Concepts:
+    - joins
+
+    Stats:
+        Runtime | 1443 ms   [Beats 76.50%]
+*/
+
+SELECT customer_id, COUNT(customer_id) AS count_no_trans
+FROM Visits AS v
+LEFT JOIN Transactions AS t
+ON t.visit_id = v.visit_id
+WHERE transaction_id IS NULL
+GROUP BY customer_id
