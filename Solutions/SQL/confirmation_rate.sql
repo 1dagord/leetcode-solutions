a8f07479d47a4827c10ccf3fbef877ddf2a010bc
Devin Gordon <dag334@cornell.edu>
2025-01-24 17:26:35 -0800

    Adding LC 1934

---
+++ Solutions/SQL/confirmation_rate.sql
@@ -0,0 +1,20 @@
+/*
+    [MEDIUM]
+    1934. Confirmation Rate
+
+    Concepts:
+    - joins
+
+    Stats:
+        Runtime | 970 ms    [Beats 57.70%]
+*/
+
+SELECT
+    s.user_id
+    , ROUND(COUNT(CASE action WHEN "confirmed" THEN 1 ELSE NULL END)
+        / COUNT(*), 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
+ORDER BY confirmation_rate
