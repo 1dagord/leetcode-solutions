9c320432262ac1064828f57079f71abeda2cba54
Devin Gordon <dag334@cornell.edu>
2025-01-24 17:31:31 -0800

    Updating LC 42

--- Solutions/Python/trapping_rain_water.py
+++ Solutions/Python/trapping_rain_water.py
@@ -7,42 +7,27 @@
     - prefix/suffix maximum
 
     Stats:
-        Runtime | 31 ms     [Beats 24.49%]
-        Memory  | 18.39 MB  [Beats 74.02%]
+        Runtime | 27 ms     [Beats 30.96%]
+        Memory  | 19.35 MB  [Beats 29.15%]
 """
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        """
-            Store prefix and suffix max 
-            (max of all indices before `i` and
-            max of all indices after `i`)
-
-            Take minimum of pref and suff to be tallest amount
-            of water possible at indes `i`
-
-            Diff of height[i] and min(pref[i], suff[i])
-            is amount of water to be stored at index `i`
-        """
         n = len(height)
         water = 0
 
-        # prefix max and suffix max
-        pref = [0]*n
-        suff = [0]*n
+        # prefix and suffix max
+        pref = height[:]
+        suff = height[:]
 
         for i in range(1, n):
-            pref[i] = max(pref[i-1], height[i-1])
+            pref[i] = max(pref[i], pref[i-1])
+            suff[n-1-i] = max(suff[n-i], suff[n-1-i])
 
-        for i in range(n - 2, -1, -1):
-            suff[i] = max(suff[i+1], height[i+1])
-
-        # for each index `i`, subtract that position's
-        # height from the min(pref[i], suff[i])
-
-        for i, h in enumerate(height):
-            if (drops := min(pref[i], suff[i]) - h) > 0:
-                water += drops
+        # ignore first and last indices
+        # as no water can be held here
+        for i in range(1, n-1):
+            water += (min(pref[i], suff[i]) - height[i])
 
         return water
     
