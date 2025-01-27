fa2bde5b7f7a7aeaef13295235aa200188dd3950
Devin Gordon <dag334@cornell.edu>
2025-01-26 17:09:29 -0800

    Updating LC 1193

--- Solutions/SQL/monthly_transactions_i.sql
+++ Solutions/SQL/monthly_transactions_i.sql
@@ -12,11 +12,13 @@
 SELECT
     CONCAT(
         YEAR(trans_date), "-", LPAD(MONTH(trans_date), 2, "0")
-    ) AS month
+        ) AS month
     , country
     , COUNT(id) AS trans_count
-    , SUM(CASE state WHEN state="approved" THEN 1 ELSE 0 END) AS approved_count
+    , SUM(CASE state WHEN state="approved" THEN 1 ELSE 0 END)
+        AS approved_count
     , SUM(amount) AS trans_total_amount
-    , SUM(CASE state WHEN state="approved" THEN amount ELSE 0 END) AS approved_total_amount
+    , SUM(CASE state WHEN state="approved" THEN amount ELSE 0 END)
+        AS approved_total_amount
 FROM Transactions
 GROUP BY country, month
