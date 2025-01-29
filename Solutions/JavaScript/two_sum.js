9b974733461d09ca4c46779b0f0d4bd31293962d
Devin Gordon <dag334@cornell.edu>
2025-01-28 18:52:20 -0800

    Creating JavaScript

---
+++ Solutions/JavaScript/two_sum.js
@@ -0,0 +1,28 @@
+/**
+ * [EASY]
+ * 1. Two Sum
+ * 
+ * Concepts:
+ * - array
+ * - hash table
+ * 
+ * Stats:
+ *      Runtime | 0 ms      [Beats 100%]
+ *      Memory  | 52.16 MB  [Beats 10.59%]
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+
+    nums.forEach((num, idx) => map.set(num, idx));
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(target - nums[i]) && i != map.get(target - nums[i])) {
+            return [i, map.get(target - nums[i])];
+        }
+    }
+
+};
