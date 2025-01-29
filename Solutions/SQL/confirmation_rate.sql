083f75dfd542307c79caf5228232d2f88a79c03b
Devin Gordon <dag334@cornell.edu>
2025-01-28 20:32:24 -0800

    Updating LC 1934

--- Solutions/SQL/confirmation_rate.sql
+++ Solutions/SQL/confirmation_rate.sql
@@ -11,10 +11,18 @@
 
 SELECT
     s.user_id
-    , ROUND(COUNT(CASE action WHEN "confirmed" THEN 1 ELSE NULL END)
-        / COUNT(*), 2) AS confirmation_rate
+    , ROUND(
+        COUNT(
+            CASE action
+            WHEN "confirmed"
+            THEN 1
+            ELSE NULL
+            END
+        ) / COUNT(*)
+        , 2
+    ) AS confirmation_rate
 FROM Signups s
 LEFT JOIN Confirmations c
-ON s.user_id = c.user_id
+    ON s.user_id = c.user_id
 GROUP BY s.user_id
 ORDER BY confirmation_rate
