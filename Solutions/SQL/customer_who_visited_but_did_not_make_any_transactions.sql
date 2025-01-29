e3fffd4434c0fb3966bcedf1da86c6b26ae2cae9
Devin Gordon <dag334@cornell.edu>
2025-01-28 20:44:16 -0800

    Updating LC 1581

--- Solutions/SQL/customer_who_visited_but_did_not_make_any_transactions.sql
+++ Solutions/SQL/customer_who_visited_but_did_not_make_any_transactions.sql
@@ -9,9 +9,11 @@
         Runtime | 1443 ms   [Beats 76.50%]
 */
 
-SELECT customer_id, COUNT(customer_id) AS count_no_trans
+SELECT
+    customer_id
+    , COUNT(customer_id) AS count_no_trans
 FROM Visits AS v
 LEFT JOIN Transactions AS t
-ON t.visit_id = v.visit_id
+    ON t.visit_id = v.visit_id
 WHERE transaction_id IS NULL
 GROUP BY customer_id
