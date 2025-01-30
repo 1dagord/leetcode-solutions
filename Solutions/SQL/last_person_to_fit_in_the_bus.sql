7ffcec92101c3bc113da5c5770e3f9543dca3dc4
Devin Gordon <dag334@cornell.edu>
2025-01-29 22:06:25 -0800

    Adding LC 1204

---
+++ Solutions/SQL/last_person_to_fit_in_the_bus.sql
@@ -0,0 +1,26 @@
+/*
+    [MEDIUM]
+    1204. Last Person to Fit in the Bus
+
+    Concepts:
+    - select
+    - joins
+
+    Stats:
+        Runtime | 1355 ms   [Beats 63.06%]
+*/
+
+WITH RunningTotal AS (
+    SELECT
+        person_id
+        , SUM(weight) OVER (order by turn) AS run_total
+    FROM Queue
+)
+SELECT
+    person_name
+FROM Queue q
+LEFT JOIN RunningTotal r
+    ON q.person_id = r.person_id
+WHERE run_total <= 1000
+ORDER BY turn DESC
+LIMIT 1
