a78bc3a1c0c6d3f32e361c75ebd35a8583a5f823
Devin Gordon <dag334@cornell.edu>
2025-02-08 10:25:04 -0800

    Adding LC 2623

---
+++ Solutions/JavaScript/memoize.js
@@ -0,0 +1,40 @@
+/*
+    [MEDIUM]
+    2623. Memoize
+
+    Concepts:
+    - function transformations
+
+    Stats:
+        Runtime | 285 ms    [Beats 69.67%]
+        Memory  | 91.64 MB  [Beats 7.61%]
+*/
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        const res = fn(...args);
+        cache.set(key, res);
+        return res;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
