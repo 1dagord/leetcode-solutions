0a2e347469799b1c631aaedb11862b6aafbb28ac
Devin Gordon <dag334@cornell.edu>
2025-02-13 22:31:52 -0800

    Adding LC 2677

---
+++ Solutions/JavaScript/chunk_array.js
@@ -0,0 +1,24 @@
+/*
+    [EASY]
+    2677. Chunk Array
+
+    Concepts:
+    - JSON
+
+    Stats:
+        Runtime | 50 ms     [Beats 84.15%]
+        Memory  | 52.26 MB  [Beats 11.70%]
+*/
+
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += size) {
+        res.push(arr.slice(i, i + size));
+    }
+    return res;
+};
