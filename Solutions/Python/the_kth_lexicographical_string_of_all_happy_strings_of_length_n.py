f5a255d03f59e20e41d2dfa5ec868fe6590df7e4
Devin Gordon <dag334@cornell.edu>
2025-02-18 18:34:24 -0800

    Adding LC 1415

---
+++ Solutions/Python/the_kth_lexicographical_string_of_all_happy_strings_of_length_n.py
@@ -0,0 +1,38 @@
+"""
+    [MEDIUM]
+    1415. The k-th Lexicographical String of All Happy Strings of Length n
+
+    Concepts:
+    - string
+    - backtracking
+
+    Stats:
+        Runtime | 5 ms      [Beats 73.52%]
+        Memory  | 18.33 MB  [Beats 32.60%]
+"""
+
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        strings = []
+
+        def solve(idx: int, string: list[str]) -> bool:
+            nonlocal strings
+
+            if idx == n:
+                strings.append("".join(string))
+                return len(strings) == k
+
+            for letter in "abc":
+                if string:
+                    if letter != string[-1]:
+                        if solve(idx + 1, string + [letter]):
+                            return True
+                else:
+                    if solve(idx + 1, string + [letter]):
+                        return True
+
+            return False
+
+        if solve(0, []):
+            return strings[-1]
+        return ""
