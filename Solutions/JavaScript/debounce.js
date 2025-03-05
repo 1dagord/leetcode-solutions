476a83c544a9aee98d64edd9a1ca54483772b747
Devin Gordon <dag334@cornell.edu>
2025-03-05 01:07:12 -0800

    Adding LC 2627

---
+++ Solutions/JavaScript/debounce.js
@@ -0,0 +1,31 @@
+/*
+    [MEDIUM]
+    2627. Debounce
+
+    Concepts:
+    - promises and time
+
+    Stats:
+        Runtime | 47 ms     [Beats 87.76%]
+        Memory  | 55.14 MB  [Beats 6.32%]
+*/
+
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn(...args), t);
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
