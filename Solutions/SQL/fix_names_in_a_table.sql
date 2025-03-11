cec9ce3be9467ded724593690c4fa5a7b4c45ddb
Devin Gordon <dag334@cornell.edu>
2025-03-10 18:46:34 -0700

    Adding LC 1667

---
+++ Solutions/SQL/fix_names_in_a_table.sql
@@ -0,0 +1,23 @@
+/*
+    [EASY]
+    1667. Fix Names in a Table
+
+    Concepts:
+    - advanced string functions
+
+    Stats:
+        Runtime | 626 ms    [Beats 96.19%]
+*/
+
+SELECT
+    user_id
+    , CONCAT(
+        UPPER(
+            SUBSTRING(name, 1, 1)
+        )
+        , LOWER(
+            SUBSTRING(name, 2, CHAR_LENGTH(name) - 1)
+        )
+    ) AS name
+FROM Users
+ORDER BY user_id
