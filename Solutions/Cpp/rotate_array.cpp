096299d2917dc0c7662752cf59c1389aa9914f99
Devin Gordon <dag334@cornell.edu>
2025-03-12 16:34:50 -0700

    Updating LC 189

--- Solutions/Cpp/rotate_array.cpp
+++ Solutions/Cpp/rotate_array.cpp
@@ -7,15 +7,13 @@
 
     Stats:
         Runtime | 0 ms      [Beats 100%]
-        Memory  | 29.61 MB  [Beats 39.40%]
+        Memory  | 29.48 MB  [Beats 79.98%]
 */
 
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k %= nums.size();
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+        int n = nums.size();
+        std::rotate(nums.begin(), nums.begin() + (n - (k % n)), nums.end());
     }
 };
