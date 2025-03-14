2a8d88da17fc3ccefc59b957089bfa24f3e20265
Devin Gordon <dag334@cornell.edu>
2025-03-14 00:52:02 -0700

    Updating LC 380

--- Solutions/Cpp/insert_delete_getrandom_o_1.cpp
+++ Solutions/Cpp/insert_delete_getrandom_o_1.cpp
@@ -12,46 +12,46 @@
 */
 
 class RandomizedSet {
-    public:
-        std::unordered_map<int, int> map = {};
-        std::vector<int> vec = {};
+public:
+    std::unordered_map<int, int> map = {};
+    std::vector<int> vec = {};
+
+    RandomizedSet() {}
     
-        RandomizedSet() {}
-        
-        bool insert(int val) {
-            if (this->map.find(val) == this->map.end()) {
-                this->map.insert({{val, this->vec.size()}});
-                this->vec.push_back(val);
-                return true;
-            }
-            return false;
+    bool insert(int val) {
+        if (map.find(val) == map.end()) {
+            map.insert({{val, vec.size()}});
+            vec.push_back(val);
+            return true;
         }
-        
-        bool remove(int val) {
-            if (this->map.find(val) != this->map.end()) {
-                // since erase is O(n), swap val with last
-                // value in vec, then pop from back in O(1)
-                int last_val = vec.back();
-                // update value
-                vec[map[val]] = vec.back();
-                vec.pop_back();
-                // update index
-                map[last_val] = map[val];
-                map.erase(val);
-                return true;
-            }
-            return false;
-        }
-        
-        int getRandom() {
-            return this->vec[rand() % this->vec.size()];
-        }
-    };
+        return false;
+    }
     
-    /**
-     * Your RandomizedSet object will be instantiated and called as such:
-     * RandomizedSet* obj = new RandomizedSet();
-     * bool param_1 = obj->insert(val);
-     * bool param_2 = obj->remove(val);
-     * int param_3 = obj->getRandom();
-     */
+    bool remove(int val) {
+        if (map.find(val) != map.end()) {
+            // since erase is O(n), swap val with last
+            // value in vec, then pop from back in O(1)
+            int last_val = vec.back();
+            // update value
+            vec[map[val]] = vec.back();
+            vec.pop_back();
+            // update index
+            map[last_val] = map[val];
+            map.erase(val);
+            return true;
+        }
+        return false;
+    }
+    
+    int getRandom() {
+        return vec[rand() % vec.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
