e9d3fd6f09ed38747094628fae57ce9e2cd4cd7b
Devin Gordon <dag334@cornell.edu>
2025-03-15 12:47:47 -0700

    Adding LC 28

---
+++ Solutions/Cpp/find_the_index_of_the_first_occurrence_in_a_string.cpp
@@ -0,0 +1,18 @@
+/*
+    [EASY]
+    28. Find the Index of the First Occurrence in a String
+
+    Concepts:
+    - string
+
+    Stats:
+        Runtime | 0 ms      [Beats 100%]
+        Memory  | 8.60 MB   [Beats 82.01%]
+*/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle);
+    }
+};
