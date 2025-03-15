7172a895efe12eab0617a932357543f22f042aa3
Devin Gordon <dag334@cornell.edu>
2025-03-15 11:10:26 -0700

    Adding LC 14

---
+++ Solutions/Cpp/longest_common_prefix.cpp
@@ -0,0 +1,57 @@
+/*
+    [EASY]
+    14. Longest Common Prefix
+
+    Concepts:
+    - string
+    - trie
+
+    Stats:
+        Runtime | 0 ms      [Beats 100%]
+        Memory  | 12.36 MB  [Beats 12.72%]
+*/
+
+class TrieNode {
+public:
+    std::unordered_map<char, TrieNode*> nodes;
+    bool is_leaf;
+
+    TrieNode() :
+        is_leaf(false),
+        nodes({})
+    {}
+};
+
+void insert(TrieNode* root, const string& key) {
+    TrieNode* curr = root;
+
+    for (char c : key) {
+        if (!curr->nodes.contains(c))
+            curr->nodes[c] = new TrieNode();
+
+        curr = curr->nodes[c];
+    }
+
+    curr->is_leaf = true;
+}
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        TrieNode* curr = new TrieNode();
+        std::string prefix = "";
+
+        for (std::string word : strs)
+            insert(curr, word);
+
+        for (char c : strs[0]) {
+            if (curr->nodes.size() > 1 || curr->is_leaf)
+                break;
+
+            prefix += c;
+            curr = curr->nodes[c];
+        }
+
+        return prefix;
+    }
+};
