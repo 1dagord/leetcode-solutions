e95179eab6a5f3f5339010e5409e779c18b443d6
Devin Gordon <dag334@cornell.edu>
2025-03-18 00:02:48 -0700

    Updating LC 56

--- Solutions/Cpp/merge_intervals.cpp
+++ Solutions/Cpp/merge_intervals.cpp
@@ -7,56 +7,30 @@
     - prefix/suffix sum
 
     Stats:
-        Runtime | 13 ms     [Beats 11.27%]
-        Memory  | 27.11 MB  [Beats 5.01%]
+        Runtime | 4 ms      [Beats 70.81%]
+        Memory  | 23.77 MB  [Beats 80.07%%]
 */
 
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = 0;
-        for (std::vector<int> row : intervals)
-            for (int item : row)
-                n = std::max(item, n);
+        if (intervals.empty()) return {};
 
+        // sort by interval start time
+        std::sort(intervals.begin(), intervals.end());
         std::vector<std::vector<int>> res = {};
-        std::unordered_set<int> singles = {};   // length-1 intervals
-        std::vector<int> pref(n+1, 0);
-        std::vector<int> suff(n+1, 0);
+        res.push_back(intervals[0]);
 
-        // mark start and end of each interval
-        for (const std::vector<int> row : intervals) {
-            const auto& [start, end] = std::array<int, 2>{row.front(), row.back()};
-            if (start == end)
-                singles.insert(start);
+        for (int i = 1; i < intervals.size(); i++) {
+            std::vector<int>& last = res.back();
 
-            pref[start]++;
-            pref[end]--;
-            suff[end]++;
-            suff[start]--;
-        }
-
-        // run prefix and suffix sum on overlaps
-        for (int i = 1; i < pref.size(); i++) {
-            pref[i] += pref[i-1];
-            suff[n-i] += suff[n-i+1];
-        }
-
-        // store merged intervals
-        std::vector<int> rng = {};
-        for (int i = 0; i < pref.size(); i++) {
-            if (pref[i] > 0 && suff[i] == 0) {
-                // begin interval
-                rng.clear();
-                rng.push_back(i);
-            } else if (pref[i] == 0 && suff[i] > 0) {
-                // end interval
-                rng.push_back(i);
-                res.push_back(rng);
-                rng.clear();
-            } else if (pref[i] == 0 && suff[i] == 0) {
-                if (singles.contains(i))
-                    res.push_back({i, i});
+            // if current interval start before last interval end...
+            if (intervals[i][0] <= last[1]) {
+                // ...merge last and current
+                last[1] = std::max(last[1], intervals[i][1]);
+            } else {
+                // create new interval
+                res.push_back(intervals[i]);
             }
         }
 
