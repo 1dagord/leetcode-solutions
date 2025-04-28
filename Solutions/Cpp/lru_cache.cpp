cc621285752b58dcd88a41d9be446d0358b28f7f
Devin Gordon <dag334@cornell.edu>
2025-04-28 15:30:29 -0700

    Updating LC 146

--- Solutions/Cpp/lru_cache.cpp
+++ Solutions/Cpp/lru_cache.cpp
@@ -8,128 +8,51 @@
     - linked list
 
     Stats:
-        Runtime | 51 ms     [Beats 95.69%]
-        Memory  | 174.82 MB [Beats 57.10%]
+        Runtime | 46 ms     [Beats 96.50%]
+        Memory  | 173.24 MB [Beats 62.89%]
 */
 
-class DLLNode {
-public:
-    int val;
-    DLLNode* left;
-    DLLNode* right;
-
-    DLLNode(int _val) {
-        val = _val;
-        left = nullptr;
-        right = nullptr;
-    }
-};
-
-class DoublyLinkedList {
-public:
-    DLLNode* head;
-    DLLNode* tail;
-    std::unordered_map<int, DLLNode*> key_to_node;
-
-    DoublyLinkedList() {
-        head = new DLLNode(-1);
-        tail = nullptr;
-        head->right = tail;
-        key_to_node = {};
-    }
-
-    void insert(int key) {
-        DLLNode* node = new DLLNode(key);
-
-        if (tail) {
-            tail->right = node;
-            node->left = tail;
-        } else {
-            head->right = node;
-            node->left = head;
-        }
-        // reset tail to current node
-        tail = node;
-        // bind key to DLLNode pointer
-        key_to_node[key] = node;
-    }
-
-    DLLNode* splice(int key) {
-        /*
-            Splices out node from doubly linked list
-        */
-        DLLNode* node = key_to_node[key];
-
-        if (node == tail) {
-            tail = tail->left;
-            return node;
-        }
-
-        node->left->right = node->right;
-        node->right->left = node->left;
-        return node;
-    }
-
-    void update(int key) {
-        /*
-            Update recency of node after use
-        */
-        // splice out node
-        DLLNode* node = splice(key);
-        
-        // append node to end of DLL
-        tail->right = node;
-        node->left = tail;
-        node->right = nullptr;
-        // set appended node as tail
-        tail = node;
-    }
-};
-
 class LRUCache {
-public:
-    int capacity;
-    std::unordered_map<int, int> cache;
-    DoublyLinkedList dll;
-
-    LRUCache(int _capacity) {
-        capacity = _capacity;
-        cache = {};
-        dll = DoublyLinkedList();
-    }
+    public:
+        int capacity;
+        std::unordered_map<int, std::list<std::pair<int, int>>::iterator> cache;
+        std::list<std::pair<int, int>> dll;
     
-    int get(int key) {
-        if (cache.contains(key)) {
-            dll.update(key);
-            return cache[key];
+        LRUCache(int _capacity) {
+            capacity = _capacity;
+            cache = {};
+            dll = {};
         }
-        return -1;
-    }
+        
+        int get(int key) {
+            if (cache.contains(key)) {
+                dll.splice(dll.begin(), dll, cache[key]);
+                return cache[key]->second;
+            }
+            return -1;
+        }
+        
+        void put(int key, int value) {
+            if (cache.contains(key)) {
+                dll.splice(dll.begin(), dll, cache[key]);
+                cache[key]->second = value;
+                return;
+            }
     
-    void put(int key, int value) {
-        if (cache.contains(key))
-            dll.update(key);
-        else
-            dll.insert(key);
-
-        // set/update key-value pair
-        cache[key] = value;
-
-        if (cache.size() > capacity) {
-            // evict lru
-            int lru = (dll.head->val == -1) ? (dll.head->right->val) : (dll.head->val);
-            cache.erase(lru);
-            // splice out lru
-            dll.splice(lru);
-            // remove mapping to node
-            dll.key_to_node.erase(lru);
+            dll.push_front({key, value});
+            cache.insert({key, dll.begin()});
+    
+            if (dll.size() > capacity) {
+                int lru = dll.back().first;
+                dll.pop_back();
+                cache.erase(lru);
+            }
         }
-    }
-};
-
-/**
-    * Your LRUCache object will be instantiated and called as such:
-    * LRUCache* obj = new LRUCache(capacity);
-    * int param_1 = obj->get(key);
-    * obj->put(key,value);
-    */
+    };
+    
+    /**
+     * Your LRUCache object will be instantiated and called as such:
+     * LRUCache* obj = new LRUCache(capacity);
+     * int param_1 = obj->get(key);
+     * obj->put(key,value);
+     */
