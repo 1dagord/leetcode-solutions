605d0018cc23dc0c267d3f94c52a9493f0188ea9
Devin Gordon <dag334@cornell.edu>
2025-06-18 20:33:17 -0700

    Updating LC 19

--- Solutions/Python/remove_nth_node_from_end_of_list.py
+++ Solutions/Python/remove_nth_node_from_end_of_list.py
@@ -8,7 +8,7 @@
 
     Stats:
         Runtime | 0 ms      [Beats 100%]
-        Memory  | 16.58 MB  [Beats 96.14%]
+        Memory  | 17.86 MB  [Beats 34.59%]
 """
 
 # Definition for singly-linked list.
@@ -18,27 +18,16 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        curr = head
-
-        # iterate n nodes into list
-        while n:
-            curr = curr.next
+        dummy = ListNode(-1, head)
+        left, right = dummy, head
+        
+        while right and n:
+            right = right.next
             n -= 1
-
-        # prepend dummy node so prev.next is node to delete
-        dummy = ListNode(None, head)
-        prev = dummy
-
-        # put prev into position
-        while curr:
-            prev = prev.next
-            curr = curr.next
-
-        # if already at end of list, remove first element
-        if prev == dummy:
-            return head.next
-
-        # splice out node
-        prev.next = prev.next.next
-
-        return head
+            
+        while right:
+            left = left.next
+            right = right.next
+            
+        left.next = left.next.next
+        return dummy.next
